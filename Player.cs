d1fde1ccf8fcf5daa8fc512ea50f2aa256d709d8
John Zhou <johnzhou2004@outlook.com>
2023-06-26 00:40:50 -0400

    Add Basic Player, Level, and Player Movement, and Supporting Packages
    Basic components on working on a FPS using Godot 4 C# with Visual Studio Code

---
+++ Player.cs
@@ -0,0 +1,107 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody3D
+{
+	public const float JumpForce = 100f;
+	public const float Sensitivity = 0.01f;
+	
+
+	public const float MoveSpeedGround = 20.0f;
+	public const float MoveSpeedAir = 18.0f;
+	
+	public const float Gravity = -4.9f;
+	public const float MaxFallSpeed = -20f;
+	public Node3D cameraBase;
+	public Camera3D camera;
+	public CharacterBody3D self;
+
+	float y_velocity=0;
+	public override void _Ready()
+
+	{
+
+		cameraBase = GetNode<Node3D>("CameraBase");	
+		camera = GetNode<Camera3D>("Camera");
+
+		Input.MouseMode = Input.MouseModeEnum.Captured; // Keeps the mouse inside of the window
+		GD.Print(cameraBase); //Camera is not accessed, it is null
+		GD.Print(cameraBase.GetType()); // Error due to calling 
+		
+
+	} 
+	public override void _Input(InputEvent @event)
+	{
+		if(@event is InputEventMouseMotion)
+		{
+			InputEventMouseMotion mouseMotion = @event as InputEventMouseMotion;
+			Vector3 cameraRot = camera.Rotation;
+			cameraRot.Y += (-mouseMotion.Relative.X * Sensitivity);
+			cameraRot.X += (-mouseMotion.Relative.Y * Sensitivity);
+			cameraRot.X = Mathf.Clamp(cameraRot.X, Mathf.DegToRad(-80f), Mathf.DegToRad(80f));
+			camera.Rotation = cameraRot;
+
+		}
+		if(@event is InputEventKey eventKey)
+		{
+			if (eventKey.IsActionPressed("exit"))
+			{
+				GetTree().Quit();
+			}
+			if (eventKey.IsActionPressed("reload"))
+			{
+				camera.Rotation = Vector3.Zero;
+			}
+		}
+
+	}
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle(); // 9.8 by default
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Vector3.Zero;
+		Boolean grounded = IsOnFloor();
+		
+		Vector3 forward_velocity =  Transform.Basis.Z * (-Input.GetActionStrength("move_forward")+Input.GetActionStrength("move_backward")); 
+		Vector3 right_velocity = Transform.Basis.X * (Input.GetActionStrength("move_right")-Input.GetActionStrength("move_left")); 
+
+		velocity = forward_velocity + right_velocity;
+		velocity = velocity.Normalized();
+		if(grounded)
+		{
+			velocity *= MoveSpeedGround;
+		}
+		else
+		{
+			velocity *= MoveSpeedAir;
+		}
+
+		//Handling gravity
+		if(grounded==false)
+		{
+			GD.Print("HERe");
+			y_velocity += Gravity;
+		}	
+		else
+		{
+			y_velocity = 0;
+			if(Input.IsActionJustPressed("jump"))
+			{
+				y_velocity = JumpForce;
+			}
+		}
+		if(y_velocity <= MaxFallSpeed)
+		{
+			y_velocity = MaxFallSpeed;
+		}
+
+		velocity.Y = y_velocity;
+		velocity = velocity.Rotated(Vector3.Up, camera.Rotation.Y);
+		Velocity = velocity;
+		MoveAndSlide();  
+		GD.Print(y_velocity);
+		GD.Print(velocity);
+		GD.Print(IsOnFloor());
+	}
+}
