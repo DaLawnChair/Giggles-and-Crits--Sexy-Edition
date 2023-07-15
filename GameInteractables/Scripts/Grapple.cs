f7fdd7e4446b6d94440b7130fe28f71196d76399
John Zhou <johnzhou2004@outlook.com>
2023-07-15 18:17:39 -0400

    Added Rope to GrappleLauncher, proper functionality to come
    -Added Rope to GrappleLauncher, a MeshInstance3D which should represent the rope that lengthens and twists to tether grappleBase and grapple.
    --Currently doesn't work due to the Rope spawning at the origin of the map, not sure why it does this
    --Also doesn't seem to lengthen as quickly as needed to tether grapple and grappleBase, despite the speed of grapple not changing

--- GameInteractables/Scripts/Grapple.cs
+++ GameInteractables/Scripts/Grapple.cs
@@ -3,13 +3,17 @@ using System;
 
 public partial class Grapple : RigidBody3D
 {
-	const float Speed = 0.01f;
+	const float Speed = 0.1f;
 	public Boolean flying = false;
+	public Vector3 hookPoint;
+	GrappleLauncher grappleLauncher;
+
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		TopLevel = true; //Prevents Grapple from moving with camera
+		grappleLauncher = (GrappleLauncher) GetParent().GetParent();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,15 +22,19 @@ public partial class Grapple : RigidBody3D
 		if(flying)
 		{			
 			ApplyImpulse(-Transform.Basis.Y*Speed);
+			grappleLauncher.drawRope();
 		}
 		else
 		{
 			Sleeping=true;
+
 		}
+		hookPoint = Position;
 	}
 
 	public void _on_area_body_3d_body_entered(Node3D body)
 	{
 		flying=false;
 	}
+	
 }
