7e4a1a0fce85937c9c976e9dbf823eb0ba895f09
John Zhou <johnzhou2004@outlook.com>
2023-07-20 18:15:52 -0400

    Fixed Rotation on Raycasts for Shotgun
    -Fixed Rotation on Raycasts for Shotgun
    -Reduced sprayPattern to 7 degrees
    -Increased damage to 10

--- Weapons/Shotgun.cs
+++ Weapons/Shotgun.cs
@@ -4,20 +4,18 @@ using System;
 public partial class Shotgun : Weapon
 {
 	Node3D rayCasts;
-	int[,] sprayPattern = new int[,] {{15,15},{15,0},{15,-15},{0,15},{0,0},{0,-15}, {-15,15},{-15,0},{-15,-15}};
+	int[,] sprayPattern = new int[,] {{7,7},{7,0},{7,-7},{0,7},{0,0},{0,-7}, {-7,7},{-7,0},{-7,-7}};
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		rayCasts = GetNode<Node3D>("RayCasts");
-		base._Ready("Shotgun", "hitscan", new int[] {6,12}, new int[] {6,36}, 3, 1, 1, 0.5f, 0.5f, 0.5f);
+		base._Ready("Shotgun", "hitscan", new int[] {6,12}, new int[] {6,36}, 10, 1, 1, 0.5f, 0.5f, 0.5f);
 		int counter=0;
 		foreach (RayCast3D rayCast in rayCasts.GetChildren())
 		{			
-			//rayCast.Rotation = new Vector3(sprayPattern[counter, 0], sprayPattern[counter, 1], 0);
-			GD.Print(rayCast.Rotation);
+			rayCast.RotationDegrees = new Vector3(sprayPattern[counter, 0], sprayPattern[counter, 1], 0);
 			counter+=1;
-			//counter = counter % 3;
 		}
 	}
 
@@ -31,7 +29,6 @@ public partial class Shotgun : Weapon
 			}
 			if(rayCast.GetCollider().HasMethod("takeDamage"))
 			{
-				GD.Print($"{rayCast.Name}");
 				rayCast.GetCollider().CallDeferred("takeDamage",damage);
 			}
 		}
